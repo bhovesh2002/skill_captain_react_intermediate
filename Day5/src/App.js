23e724624ca0f37430ff61b44081813c45090528
bhovesh2002 <110907973+bhovesh2002@users.noreply.github.com>
2024-07-07 23:39:26 +0530

    Add files via upload

---
+++ Day5/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import LoginForm from './components/LoginForm';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Welcome to Our Interactive Login Page</h1>
+      <LoginForm />
+    </div>
+  );
+}
+
+export default App;
